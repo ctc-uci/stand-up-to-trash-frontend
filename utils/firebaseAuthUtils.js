04ec510577ca3b652990e77f337259ceee5813ca
Ostend Suryajaya <osuryaja@uci.edu>
2023-12-03 13:19:45 -0800

    Added config for Firebase Auth

---
+++ utils/firebaseAuthUtils.js
@@ -0,0 +1,18 @@
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_FIREBASE_APIKEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECTID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGINGSENDERID,
+  appId: import.meta.env.VITE_FIREBASE_APPID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENTID,
+};
+
+const app = initializeApp(firebaseConfig);
+// eslint-disable-next-line no-unused-vars
+const auth = getAuth(app);
+
+// Don't forget to export your functions!
