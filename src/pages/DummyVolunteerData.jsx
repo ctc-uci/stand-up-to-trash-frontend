bf3db2245bc112ed592b3221f8e37920f8fa66ca
Emmy Chen <emmyc3@uci.edu>
2024-01-10 13:58:58 -0800

    Committing initial

--- src/pages/DummyVolunteerData.jsx
+++ src/pages/DummyVolunteerData.jsx
@@ -1,6 +1,64 @@
+/* eslint-disable  no-unused-vars */
+import Backend from '../../utils/utils';
+import React, { useEffect, useState } from 'react';
+import { Heading, Text, SimpleGrid, Button, Card, CardHeader, CardBody, CardFooter } from '@chakra-ui/react'
+
+
+
 
 const DummyVolunteerData = () => {
-  return <p>Placeholder for the volunteer data page</p>;
+
+  const [volunteers, setVolunteers] = useState([]);
+
+  const getVolunteerData = async () => {
+    const { data } = await Backend.get('/data');
+    console.log(data);
+    setVolunteers(data);
+  };
+
+  useEffect(() => {
+    getVolunteerData();
+    console.log(volunteers);
+  }, []);
+  return (
+      <>
+        <SimpleGrid spacing={4} templateColumns='repeat(auto-fill, minmax(200px, 1fr))'>
+        <Card>
+          <CardHeader>
+            <Heading size='md'> Something </Heading>
+          </CardHeader>
+          <CardBody>
+            <Text>View a summary of all your customers over the last month.</Text>
+          </CardBody>
+          <CardFooter>
+            <Button>View here</Button>
+          </CardFooter>
+        </Card>
+        <Card>
+          <CardHeader>
+            <Heading size='md'> Customer dashboard</Heading>
+          </CardHeader>
+          <CardBody>
+            <Text>View a summary of all your customers over the last month.</Text>
+          </CardBody>
+          <CardFooter>
+            <Button>View here</Button>
+          </CardFooter>
+        </Card>
+        <Card>
+          <CardHeader>
+            <Heading size='md'> Customer dashboard</Heading>
+          </CardHeader>
+          <CardBody>
+            <Text>View a summary of all your customers over the last month.</Text>
+          </CardBody>
+          <CardFooter>
+            <Button>Delete</Button>
+          </CardFooter>
+        </Card>
+      </SimpleGrid>
+      </>
+    );
 };
 
 export default DummyVolunteerData;
