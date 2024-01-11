0e1ebc434ca7bb37e0f3846b70dc0c312ee7ced1
Brendan Lieu <brendanlieu05@gmail.com>
2024-01-10 17:12:11 -0800

    Created Chakra card skeleton with database pulls and changed utils.js credentials

--- utils/utils.js
+++ utils/utils.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const Backend = axios.create({
   baseURL: "http://localhost:3001",
-  withCredentials: true,
+  withCredentials: false, //previously true but was giving me problems
 });
 
 export default Backend;
