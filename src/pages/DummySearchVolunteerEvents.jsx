043ae591c767adfefbade7b04e536527dae4910e
Matthew <deadwalker1228@gmail.com>
2024-01-10 21:15:11 -0800

    Setup joined db

--- src/pages/DummySearchVolunteerEvents.jsx
+++ src/pages/DummySearchVolunteerEvents.jsx
@@ -1,5 +1,19 @@
+//import eventsRouter from "../../../stand-up-to-trash-backend/routes/events";
+//import dataRouter from "../../../stand-up-to-trash-backend/routes/data";
+import { Select } from "@chakra-ui/react";
+
+
+
+
 const DummySearchVolunteerEvents = () => {
-  return <p>Placeholder for the search volunteer events page</p>;
+  return( <Select placeholder="Select Event">
+    <option>hio</option>
+    <option>he</option>
+    <option>ji</option>
+  </Select>)
+  
+  
 };
 
 export default DummySearchVolunteerEvents;
+
