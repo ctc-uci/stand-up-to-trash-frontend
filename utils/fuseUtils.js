c6573b7a3bc420d217b01f191a01e1d931e2417a
Matthew <deadwalker1228@gmail.com>
2024-01-14 14:35:42 -0800

    blah

--- utils/fuseUtils.js
+++ utils/fuseUtils.js
@@ -1,10 +1,17 @@
 import Backend from './utils.js';
 
+/* 
+  fetches all the events data
+*/
 const fetchEvents = async () => {
   const data = await Backend.get('/events');
   return data.data;
 };
 
+
+/*
+    fetches joined data from all tables
+*/
 const fetchJoinedEvents = async () => {
   const data = await Backend.get('/events/joined');
   return data.data;
