9edcad23b3ddf62e1e6fbd5d9026345cff2e6554
Phillip Cutter <phillipbcutter@gmail.com>
2024-01-21 00:50:20 -0800

    Complete feature 1 for all event stats

--- src/pages/DummyStatsPage.jsx
+++ src/pages/DummyStatsPage.jsx
@@ -1,7 +1,31 @@
-import { Text } from '@chakra-ui/react';
+import { Card, CardBody, Text } from '@chakra-ui/react';
+import { useEffect, useState } from 'react';
+import Backend from '../utils/utils';
 
 const DummyStatsPage = () => {
-  return <Text>Placeholder for Stats Page</Text>;
+  const [stats, setStats] = useState(null);
+  useEffect(() => {
+    getVolunteerData().then(data => setStats(data));
+  }, []);
+  return (
+    <Card m="8">
+      <CardBody>
+        <Text>
+          All events
+          <br />
+          <br />
+          total trash:
+          <br />
+          {stats} pounds
+        </Text>
+      </CardBody>
+    </Card>
+  );
+};
+
+const getVolunteerData = async () => {
+  const resp = await Backend.get('/stats');
+  return resp.data;
 };
 
 export default DummyStatsPage;
