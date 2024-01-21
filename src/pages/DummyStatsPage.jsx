241cba0f9d0bd289578981e08942d9faad2064af
Phillip Cutter <phillipbcutter@gmail.com>
2024-01-21 13:03:12 -0800

    Finish feature 1 & 2 to select data by event or volunteer

--- src/pages/DummyStatsPage.jsx
+++ src/pages/DummyStatsPage.jsx
@@ -1,30 +1,160 @@
-import { Card, CardBody, Text } from '@chakra-ui/react';
-import { useEffect, useState } from 'react';
+import { Box, Button, Card, CardBody, HStack, Heading, Select, Text } from '@chakra-ui/react';
+import PropTypes from 'prop-types';
+import { useEffect, useRef, useState } from 'react';
 import Backend from '../utils/utils';
 
 const DummyStatsPage = () => {
+  return (
+    <>
+      <AllStats />
+      <StatsByEvent />
+      <StatsByProfile />
+    </>
+  );
+};
+
+const AllStats = () => {
   const [stats, setStats] = useState(null);
   useEffect(() => {
-    getVolunteerData().then(data => setStats(data));
+    getStatsData().then(data => setStats(data));
   }, []);
   return (
-    <Card m="8">
+    <Box m="8">
+      <Heading>All trash</Heading>
+      <VolunteerTrashCollectedCard title="Total Trash Collected" amount={stats} />
+    </Box>
+  );
+};
+
+const StatsByEvent = () => {
+  const [stats, setStats] = useState(null);
+
+  const [events, setEvents] = useState([]);
+  const [selectedEvent, setSelectedEvent] = useState(null);
+  const select = useRef(null);
+  useEffect(() => {
+    getEvents().then(data => setEvents(data));
+  }, []);
+
+  return (
+    <Box m="8">
+      <Heading>Trash by event</Heading>
+      <HStack mt="2">
+        <Select placeholder="Select an event" ref={select}>
+          {events.map(event => (
+            <option value={event.id} key={event.id}>
+              {event.name}
+            </option>
+          ))}
+        </Select>
+        <Button
+          onClick={() => {
+            setSelectedEvent(
+              events.filter(event => `${event.id}` === `${select.current.value}`)[0],
+            );
+            getEventStats(select.current.value).then(data => {
+              setStats(data);
+            });
+          }}
+        >
+          View Data
+        </Button>
+      </HStack>
+      {!selectedEvent ? (
+        <Text mt="4">Select an event to view data</Text>
+      ) : (
+        <VolunteerTrashCollectedCard title={selectedEvent.name} amount={stats} />
+      )}
+    </Box>
+  );
+};
+
+const StatsByProfile = () => {
+  const [stats, setStats] = useState(null);
+
+  const [profiles, setProfiles] = useState([]);
+  const [selectedProfile, setSelectedProfile] = useState(null);
+  const select = useRef(null);
+  useEffect(() => {
+    getProfiles().then(data => setProfiles(data));
+  }, []);
+
+  return (
+    <Box m="8">
+      <Heading>Trash by profile</Heading>
+      <HStack mt="2">
+        <Select placeholder="Select a profile" ref={select}>
+          {profiles.map(profile => (
+            <option value={profile.id} key={profile.id}>
+              {profile.first_name} (ID:{profile.id})
+            </option>
+          ))}
+        </Select>
+        <Button
+          onClick={() => {
+            setSelectedProfile(profiles.filter(profile => profile.id === select.current.value)[0]);
+            getVolunteerStats(select.current.value).then(data => {
+              setStats(data);
+            });
+          }}
+        >
+          View Data
+        </Button>
+      </HStack>
+      {!selectedProfile ? (
+        <Text mt="4">Select a profile to view data</Text>
+      ) : (
+        <VolunteerTrashCollectedCard title={selectedProfile.first_name} amount={stats} />
+      )}
+    </Box>
+  );
+};
+
+const VolunteerTrashCollectedCard = ({ title, amount }) => {
+  return (
+    <Card my="4">
       <CardBody>
         <Text>
-          All events
+          {title}
           <br />
           <br />
           total trash:
           <br />
-          {stats} pounds
+          {amount} pounds
         </Text>
       </CardBody>
     </Card>
   );
 };
 
-const getVolunteerData = async () => {
-  const resp = await Backend.get('/stats');
+VolunteerTrashCollectedCard.propTypes = {
+  title: PropTypes.string.isRequired,
+  amount: PropTypes.number.isRequired,
+};
+
+const getStatsData = async dataId => {
+  const resp = await Backend.get(dataId ? `/stats/${dataId}` : '/stats');
+  return resp.data;
+};
+
+const getEventStats = async dataId => {
+  const resp = await Backend.get(`/stats/event/${dataId}`);
+  return resp.data;
+};
+
+const getVolunteerStats = async dataId => {
+  const resp = await Backend.get(`/stats/volunteer/${dataId}`);
+  return resp.data;
+};
+
+const getEvents = async () => {
+  const resp = await Backend.get('/events');
+  console.log(resp.data);
+  return resp.data;
+};
+
+const getProfiles = async () => {
+  const resp = await Backend.get('/profiles');
   return resp.data;
 };
 
