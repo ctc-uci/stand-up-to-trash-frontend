bd177f5509a4262756e4631368712978add275a4
Kevin Wu <kevinwu098@gmail.com>
2024-02-09 22:30:11 -0800

    feat: display signed in user in playground

--- src/components/Playground/Playground.jsx
+++ src/components/Playground/Playground.jsx
@@ -1,16 +1,32 @@
 import ExportButton from '../ExportCSVButton/ExportButton';
 import AddEventsModal from '../AddEventsModal/AddEventsModal';
 import Dropzone from '../Dropzone.tsx';
+import { getAuth, onAuthStateChanged } from 'firebase/auth';
+import { useEffect, useState } from 'react';
+
+const auth = getAuth();
 
 const Playground = () => {
+  const [user, setUser] = useState(auth.currentUser);
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, user => {
+      setUser(user);
+    });
+
+    return () => unsubscribe();
+  }, []);
+
   return (
     <div>
       <p>Use this page to test out the look of any of your components!</p>
+      <p>{user?.email}</p>
+
       <ExportButton eventId={19} />
       <ExportButton eventId={-1} />
 
-      <AddEventsModal/>
-      <Dropzone/>
+      <AddEventsModal />
+      <Dropzone />
     </div>
   );
 };
