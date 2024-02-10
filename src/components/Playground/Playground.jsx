17512ca21f990f29d937434e6cfbae45f0f3ed93
Nathan Pietrantonio <pietrann@uci.edu>
2024-02-10 10:25:47 -0800

    Match design for event modal

--- src/components/Playground/Playground.jsx
+++ src/components/Playground/Playground.jsx
@@ -1,17 +1,18 @@
 import ExportButton from '../ExportCSVButton/ExportButton';
 import AddEventsModal from '../AddEventsModal/AddEventsModal';
 import Dropzone from '../Dropzone.tsx';
+import { Flex } from '@chakra-ui/react';
 
 const Playground = () => {
   return (
-    <div>
+    <Flex backgroundColor={"pink"} h={"100vh"} w={"100vw"} flexDir={"column"}>
       <p>Use this page to test out the look of any of your components!</p>
       <ExportButton eventId={19} />
       <ExportButton eventId={-1} />
 
       <AddEventsModal/>
       <Dropzone/>
-    </div>
+    </Flex>
   );
 };
 
