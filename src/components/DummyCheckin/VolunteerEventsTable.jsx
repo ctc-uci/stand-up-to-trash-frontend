850ff078cbaab252564e5b1dc92ca437c7698d18
bobbyzhong <3bobbyzhong3@gmail.com>
2024-02-10 18:55:58 -0800

    modified to display all users and clickable action;

--- src/components/DummyCheckin/VolunteerEventsTable.jsx
+++ src/components/DummyCheckin/VolunteerEventsTable.jsx
@@ -15,89 +15,100 @@ import {
   TableContainer,
   Tag,
   Menu,
+  useDisclosure,
 } from '@chakra-ui/react';
-import { FaUser, FaClock, FaArrowUp, FaTag } from "react-icons/fa";
-import { BsThreeDots } from "react-icons/bs";
+import { FaUser, FaClock, FaArrowUp, FaTag } from 'react-icons/fa';
+import { BsThreeDots } from 'react-icons/bs';
+import DataEntryModal from '../DataEntryModal/DataEntryModal';
 // import DataEntryModal from '../DataEntryModal/DataEntryModal';
 
 const VolunteerEventsTable = ({ volunteers }) => {
-  //console.log(volunteers);
-
-  const renderVolunteerRow = volunteer => {
-    const {first_name, last_name, email, image_url, is_checked_in } = volunteer.props.data;
-    console.log(volunteer)
+  const RenderVolunteerRow = volunteer => {
+    const { isOpen, onOpen, onClose } = useDisclosure();
+    const { first_name, last_name, email, image_url, is_checked_in } = volunteer;
+    console.log(volunteer);
     // lofi says no checkboxes, so names and the name column title has a margin left of 5rem, feel free to remove if checkboxes added
     return (
       <Tr>
         <Td>
           <Flex ml="5rem">
-            <Image src={image_url} boxSize="4rem" borderRadius='full'/>
+            <Image src={image_url} boxSize="4rem" borderRadius="full" />
             <Flex direction="column" ml={3} mt={2} g={1}>
-              <Text>{first_name} {last_name}</Text>
+              <Text>
+                {first_name} {last_name}
+              </Text>
               <Text fontWeight="light">{email}</Text>
             </Flex>
           </Flex>
         </Td>
         <Td>
-          <Text color="green" fontWeight="650">ATTENDED</Text>
+          <Text color="green" fontWeight="650">
+            ATTENDED
+          </Text>
         </Td>
         <Td>
           <Flex>
-            {
-              is_checked_in ? <Tag bg="#D53F8C"  color="white">Input Data</Tag> : <Tag bg="#95D497">Check-In</Tag>
-            }
+            {is_checked_in ? (
+              <>
+                <Tag cursor={'pointer'} onClick={onOpen} bg="#D53F8C" color="white">
+                  Input Data
+                </Tag>
+                <DataEntryModal isOpen={isOpen} onClose={onClose} />
+              </>
+            ) : (
+              <Tag bg="#95D497">Check-In</Tag>
+            )}
             <Spacer />
             <Menu>
-            <BsThreeDots />
+              <BsThreeDots />
             </Menu>
           </Flex>
-
         </Td>
       </Tr>
-
     );
-  }
+  };
 
   return (
     <TableContainer>
-      <Table variant='simple'>
+      <Table variant="simple">
         <TableCaption>Volunteers for this event</TableCaption>
         <Thead bg="#F7FAFC">
           <Tr>
             <Th w="65%">
               <Flex gap={2} ml="5rem">
-                  <FaUser size="1rem"/>
-                  <Text color="black" fontWeight="650">NAME</Text>
+                <FaUser size="1rem" />
+                <Text color="black" fontWeight="650">
+                  NAME
+                </Text>
               </Flex>
             </Th>
             <Th>
               <Flex gap={2}>
-                  <FaClock size="1rem"/>
-                  <Text color="black" fontWeight="650">STATUS</Text>
-                  <FaArrowUp size="1rem"/>
+                <FaClock size="1rem" />
+                <Text color="black" fontWeight="650">
+                  STATUS
+                </Text>
+                <FaArrowUp size="1rem" />
               </Flex>
             </Th>
             <Th>
               <Flex gap={2}>
-                  <FaTag size="1rem" transform="rotate(90)"/>
-                  <Text color="black" fontWeight="650">ACTION</Text>
+                <FaTag size="1rem" transform="rotate(90)" />
+                <Text color="black" fontWeight="650">
+                  ACTION
+                </Text>
               </Flex>
             </Th>
           </Tr>
         </Thead>
-        <Tbody>
-            {
-              volunteers.map((volunteer) => renderVolunteerRow(volunteer))
-            }
-        </Tbody>
+        <Tbody>{volunteers.map(volunteer => RenderVolunteerRow(volunteer))}</Tbody>
       </Table>
     </TableContainer>
   );
 };
 
-
 // VolunteerEventsTable.propTypes = {
 //   volunteers: PropTypes.isObject.isRequired
 // };
 
-export default VolunteerEventsTable
+export default VolunteerEventsTable;
--- src/pages/DummyCheckin.jsx
+++ src/pages/DummyCheckin.jsx
@@ -1,46 +1,40 @@
 /* eslint-disable react/prop-types */
 // import { useState, useEffect, useCallback } from 'react';
 import { useState, useEffect } from 'react';
-import { useParams } from 'react-router-dom';
 import { Input } from '@chakra-ui/react';
-import { fetchJoinedEvents } from '../utils/fuseUtils';
-import {
-  Container,
-  Flex,
-  Button,
-  Box,
-} from '@chakra-ui/react';
-import Fuse from 'fuse.js';
-import JoinedDataContainer from '../components/DummySearchVolunteerEvents/JoinedDataContainer';
+// import { fetchJoinedEvents } from '../utils/fuseUtils';
+import { Container, Flex, Button, Box } from '@chakra-ui/react';
+// import JoinedDataContainer from '../components/DummySearchVolunteerEvents/JoinedDataContainer';
 import VolunteerEventsTable from '../components/DummyCheckin/VolunteerEventsTable';
+import Backend from '../utils/utils';
 // import Backend from '../utils/utils';
 // import { useDisclosure } from '@chakra-ui/react';
 
 const DummyCheckin = () => {
   // const { isOpen, onOpen, onClose } = useDisclosure();
-  const [joinedData, setJoinedData] = useState([]);
-  const [searchResults, setSearchResults] = useState([]);
+  // const [joinedData, setJoinedData] = useState([]);
+  // const [searchResults, setSearchResults] = useState([]);
   const [volunteerResults, setVolunteerResults] = useState([]);
   // const [checkedInVolunteers, setCheckedInVolunteers] = useState([]);
   // const [notCheckedInVolunteers, setNotCheckedInVolunteers] = useState([]);
   const [input, setInput] = useState('');
-  const { eventId } = useParams();
+  // const { eventId } = useParams();
   const [showCheckedIn, setShowCheckedIn] = useState(false);
 
   /*
     Filters on change to joinedData which it relies on, only really necessary once but needs to happen aftr joinedData complete
   */
-  useEffect(() => {
-    const filterHandler = () => {
-      const filterdData = joinedData.filter(item => {
-        if (item.props.data.event_id == eventId || eventId == -1) {
-          return true;
-        }
-      });
-      setSearchResults(filterdData);
-    };
-    filterHandler();
-  }, [joinedData, eventId]);
+  // useEffect(() => {
+  //   const filterHandler = () => {
+  //     const filterdData = joinedData.filter(item => {
+  //       if (item.props.data.event_id == eventId || eventId == -1) {
+  //         return true;
+  //       }
+  //     });
+  //     setSearchResults(filterdData);
+  //   };
+  //   filterHandler();
+  // }, [joinedData, eventId]);
 
   /*
     Dynamically re-renders volunteer entries when a user checks them in
@@ -137,31 +131,40 @@ const DummyCheckin = () => {
   /*
     This useEffect is for fetching all the events and JOINED events/volunteers/events_data data
   */
-  useEffect(() => {
-    const setData = async () => {
-      // await fetchEvents().then(data => setEventsData(data));
-      await fetchJoinedEvents().then(data => {
-        const joinedContainers = data.map(event => {
-          return <JoinedDataContainer data={event} key={event.volunteer_id} />;
-        });
-        setJoinedData(joinedContainers);
-      });
-    };
-    setData();
-  }, []);
+  // useEffect(() => {
+  //   const setData = async () => {
+  //     // await fetchEvents().then(data => setEventsData(data));
+  //     await fetchJoinedEvents().then(data => {
+  //       const joinedContainers = data.map(event => {
+  //         return <JoinedDataContainer data={event} key={event.volunteer_id} />;
+  //       });
+  //       setJoinedData(joinedContainers);
+  //     });
+  //   };
+  //   setData();
+  // }, []);
 
   /*
     This useffect is used for updating the display volunteer data based on user input utilizing fuzzy search
   */
+  // useEffect(() => {
+  //   const options = {
+  //     keys: ['props.data.first_name', 'props.data.last_name', 'props.data.email'],
+  //   };
+  //   const fuse = new Fuse(searchResults, options);
+  //   const searchResult = fuse.search(input);
+  //   const reduceResult = searchResult.map(result => result.item);
+  //   setVolunteerResults(reduceResult);
+  // }, [input, searchResults]);
   useEffect(() => {
-    const options = {
-      keys: ['props.data.first_name', 'props.data.last_name', 'props.data.email'],
+    const fetchData = async () => {
+      const data = await Backend.get('/events/joined');
+      const volunteers = data.data;
+
+      setVolunteerResults(volunteers);
     };
-    const fuse = new Fuse(searchResults, options);
-    const searchResult = fuse.search(input);
-    const reduceResult = searchResult.map(result => result.item);
-    setVolunteerResults(reduceResult);
-  }, [input, searchResults]);
+    fetchData();
+  }, []);
 
   const handleButtonClick = () => {
     setShowCheckedIn(!showCheckedIn);
@@ -246,7 +249,7 @@ const DummyCheckin = () => {
                 />
               ))
             : '')} */}
-            <VolunteerEventsTable volunteers={volunteerResults}/>
+        <VolunteerEventsTable volunteers={volunteerResults} />
       </Container>
     </>
   );
