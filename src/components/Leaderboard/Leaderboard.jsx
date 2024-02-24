145159ec1240b2b9c9e4fd0c9a7e0ceed8ed3a3f
Gayathri Yedavilli <gyedavil@uci.edu>
2024-02-24 12:15:44 -0800

    finish leaderboard for specific event

--- src/components/Leaderboard/Leaderboard.jsx
+++ src/components/Leaderboard/Leaderboard.jsx
@@ -1,52 +1,131 @@
-import { Box, Card, Text } from '@chakra-ui/react';
+import { Box, Card, Text, Select } from '@chakra-ui/react';
 import { useState, useEffect } from 'react';
+import PropTypes from 'prop-types';
 import Backend from '../../utils/utils.js';
 
 const LeaderboardCard = () => {
-    const [topThree, setTopThree] = useState([]);
+  const [topThree, setTopThree] = useState([]);
+  const [events, setEvents] = useState([]);
+  const [topThreeSpecificEvent, setEventSelect] = useState([]);
+  const [selectedEvent, setSelectedEvent] = useState(-1);
 
-    const getTopThree = async () => {
-        try {
-          const topThreeData = await Backend.get('/stats/leaderboard');
-          console.log(topThreeData);
-          setTopThree(topThreeData.data);
-        } catch (err) {
-          console.log(`Error getting top three volunteers: `, err.message);
-        }
-    };
+  const getEvents = async () => {
+    try {
+      const events = await Backend.get('/events');
+      setEvents(events.data);
+    } catch (err) {
+      console.log(`Error getting top three volunteers: `, err.message);
+    }
+  };
 
-    const truncate = (num, decimalPlaces) => {
-        const factor = Math.pow(10, decimalPlaces);
-        return Math.floor(num * factor) / factor;
-    };    
+  const getTopThree = async () => {
+    try {
+      const topThreeData = await Backend.get('/stats/leaderboard');
+      setTopThree(topThreeData.data);
+    } catch (err) {
+      console.log(`Error getting top three volunteers: `, err.message);
+    }
+  };
 
-    useEffect(() => {
-        getTopThree();
-    }, []);
+  const truncate = (num, decimalPlaces) => {
+    const factor = Math.pow(10, decimalPlaces);
+    return Math.floor(num * factor) / factor;
+  };
+
+  const handleChange = event => {
+    setSelectedEvent(event.target.value);
+    console.log(selectedEvent);
+  };
+
+  const SelectEvent = () => {
+    return (
+      <>
+        <Select placeholder="Select Event" onChange={e => handleChange(e)}>
+          {events.map(event => (
+            <option key={event.id} value={event.id}>
+              {event.name}
+            </option>
+          ))}
+        </Select>
+      </>
+    );
+  };
+
+  const getTopThreeSpecificEvent = async eventID => {
+    try {
+      const topThreeSpecificEvent = await Backend.get(`/stats/leaderboard/${eventID}`);
+      setEventSelect(topThreeSpecificEvent.data);
+    } catch (err) {
+      console.log(`Error getting top three volunteers: `, err.message);
+    }
+  };
+
+  const ActualLeaderboard = ({ LeaderboardArray }) => {
+    return (
+      <>
+        <Box
+          width="331px"
+          height="195px"
+          mx="13px"
+          my="19px"
+          display="flex"
+          flexDir="column"
+          alignContent="center"
+          justifyContent="center"
+          gap="9px"
+          h="100%"
+        >
+          <Text fontWeight="bold" decoration="underline" textAlign="center">
+            top 3 volunteers
+          </Text>
+          <Box mx="23px" bgColor="#2D558A" color="white" borderRadius="60px" padding="2px">
+            <Text fontWeight="bold" color="white" textAlign="center">
+              {LeaderboardArray[0] && LeaderboardArray[0].volunteer_first_name}{' '}
+              {LeaderboardArray[0] && LeaderboardArray[0].volunteer_last_name} ...........{' '}
+              {LeaderboardArray[0] && truncate(LeaderboardArray[0].total_weight, 2)} lbs
+            </Text>
+          </Box>
+          <Text fontWeight="bold" textAlign="center">
+            {LeaderboardArray[1] && LeaderboardArray[1].volunteer_first_name}{' '}
+            {LeaderboardArray[1] && LeaderboardArray[1].volunteer_last_name} ............{' '}
+            {LeaderboardArray[1] && truncate(LeaderboardArray[1].total_weight, 2)} lbs
+          </Text>
+          <Text fontWeight="bold" textAlign="center">
+            {LeaderboardArray[2] && LeaderboardArray[2].volunteer_first_name}{' '}
+            {LeaderboardArray[2] && LeaderboardArray[2].volunteer_last_name} ...........{' '}
+            {LeaderboardArray[2] && truncate(LeaderboardArray[2].total_weight, 2)} lbs
+          </Text>
+        </Box>
+      </>
+    );
+  };
+
+  ActualLeaderboard.propTypes = {
+    LeaderboardArray: PropTypes.array.isRequired,
+  };
+
+  useEffect(() => {
+    getEvents();
+  }, []);
+
+  useEffect(() => {
+    getTopThree();
+  }, []);
+
+  useEffect(() => {
+    getTopThreeSpecificEvent(selectedEvent);
+  }, [selectedEvent]);
 
   return (
-    <> 
-        <Card borderRadius='0px 15px 15px 0px'>
-            <Box
-                width='331px'
-                height='195px'
-                mx="13px"
-                my="19px"
-                display="flex"
-                flexDir="column"
-                alignContent="center"
-                justifyContent="center"
-                gap="9px"
-                h="100%"
-            >
-                <Text fontWeight='bold' decoration='underline' textAlign='center'>top 3 volunteers</Text>
-                <Box mx='23px' bgColor='#2D558A' color='white' borderRadius='60px' padding='2px'>
-                    <Text fontWeight='bold' color='white' textAlign='center'>{topThree[0] && topThree[0].volunteer_first_name} {topThree[0] && topThree[0].volunteer_last_name} ........... {topThree[0] && truncate(topThree[0].total_weight, 2)} lbs</Text>
-                </Box>
-                <Text fontWeight='bold' textAlign='center'>{topThree[1] && topThree[1].volunteer_first_name} {topThree[1] && topThree[1].volunteer_last_name} ............ {topThree[1] && truncate(topThree[1].total_weight, 2)} lbs</Text>
-                <Text fontWeight='bold' textAlign='center'>{topThree[2] && topThree[2].volunteer_first_name} {topThree[2] && topThree[2].volunteer_last_name} ........... {topThree[2] && truncate(topThree[2].total_weight, 2)} lbs</Text>
-            </Box>
-        </Card>
+    <>
+      <Card borderRadius="0px 15px 15px 0px">
+        <SelectEvent />
+        {selectedEvent === -1 ? (
+          <ActualLeaderboard LeaderboardArray={topThree} />
+        ) : (
+          <ActualLeaderboard LeaderboardArray={topThreeSpecificEvent} />
+        )}
+      </Card>
     </>
   );
 };
