c9e85e470e6afb7dbe1203e0df7d0469e73fbd2c
Kevin Wu <kevinwu098@gmail.com>
2024-03-01 14:18:09 -0800

    fix: return nothing while checking auth

--- src/utils/ProtectedRoute.jsx
+++ src/utils/ProtectedRoute.jsx
@@ -1,4 +1,4 @@
-import { useContext, useEffect } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import RoleContext from './RoleContext';
 import { useNavigate } from 'react-router-dom';
 
@@ -8,37 +8,54 @@ const ProtectedRoute = ({ children, pageType }) => {
   console.log(`The current user's role is: ${role}`);
   console.log(`The type of page is: ${pageType}`);
 
+  const [loading, setLoading] = useState(true);
+
   useEffect(() => {
-    if (pageType === 'admin') {
-      if (role === 'admin') {
-        return children;
-      } else if (role === 'volunteer') {
-        navigate('/playground');
-      } else if (role === 'unloggedIn') {
-        navigate('/loginv2');
+    const isAuthenticated = () => {
+      if (pageType === 'admin') {
+        if (role === 'admin') {
+          setLoading(true);
+        } else if (role === 'volunteer') {
+          navigate('/playground');
+          setLoading(false);
+        } else if (role === 'unloggedIn') {
+          navigate('/loginv2');
+          setLoading(false);
+        }
       }
-    }
 
-    if (pageType === 'volunteer') {
-      if (role === 'admin') {
-        return children;
-      } else if (role === 'volunteer') {
-        return children;
-      } else if (role === 'unloggedIn') {
-        navigate('/loginv2');
+      if (pageType === 'volunteer') {
+        if (role === 'admin') {
+          setLoading(true);
+        } else if (role === 'volunteer') {
+          setLoading(true);
+        } else if (role === 'unloggedIn') {
+          navigate('/loginv2');
+          setLoading(false);
+        }
       }
-    }
 
-    if (pageType === 'authentication') {
-      if (role === 'admin') {
-        navigate('/');
-      } else if (role === 'volunteer') {
-        navigate('/playground');
-      } else if (role === 'unloggedIn') {
-        return children;
+      if (pageType === 'authentication') {
+        if (role === 'admin') {
+          navigate('/');
+          setLoading(false);
+        } else if (role === 'volunteer') {
+          navigate('/playground');
+          setLoading(false);
+        } else if (role === 'unloggedIn') {
+          setLoading(true);
+        }
       }
-    }
+    };
+
+    isAuthenticated();
   }, [children, navigate, pageType, role]);
+
+  if (loading) {
+    return;
+  }
+
+  return children;
 };
 
 export default ProtectedRoute;
