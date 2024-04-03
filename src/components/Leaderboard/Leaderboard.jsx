4abc2b2ef03b81d70f0f8aaaec0706edd3d98ddc
Emmy Chen <emmyc3@uci.edu>
2024-04-03 16:00:20 -0700

    Committing mobile view changes

--- src/components/Leaderboard/Leaderboard.jsx
+++ src/components/Leaderboard/Leaderboard.jsx
@@ -30,16 +30,16 @@ const LeaderboardCard = ({ event_id }) => {
     return (
       <>
         <Flex
-          mx="13px"
+          mx="5px"
           my="19px"
           display="flex"
           flexDir="column"
           alignContent="center"
           justifyContent="center"
-          gap="5px"
+          gap={{ base: '2px', xl: '5px' }}
           h="100%"
         >
-          <Text fontSize={'3xl'} fontWeight="sm" textAlign="center">
+          <Text fontSize={{ base: '22px', xl: '3xl' }} fontWeight="sm" textAlign="center">
             Leaderboard
           </Text>
 
@@ -48,17 +48,22 @@ const LeaderboardCard = ({ event_id }) => {
             bgColor="#D4E4F9"
             color="#1873FB"
             borderRadius={'md'}
-            px={3}
+            px={{ base: 2, xl: 3 }}
             py={2}
             alignItems={'center'}
             gap={3}
-            fontSize={'lg'}
+            fontSize={{ base: '16px', xl: 'lg' }}
           >
-            <Flex gap={4} alignItems={'center'} w={'9rem'}>
-              <Flex justifyContent={'center'} w={'30px'}>
+            <Flex gap={2} alignItems={'center'} w={'9rem'}>
+              <Flex justifyContent={'center'} w={{ base: '12px', xl: '30px' }}>
                 <Image src={first} />
               </Flex>
-              <Text fontWeight="bold" textAlign="center">
+              <Text
+                fontWeight="bold"
+                fontSize={{ base: '16px', xl: 'lg' }}
+                w="auto"
+                textAlign="center"
+              >
                 {LeaderboardArray[0] && LeaderboardArray[0].volunteer_first_name}{' '}
                 {LeaderboardArray[0] && LeaderboardArray[0].volunteer_last_name}
               </Text>
@@ -72,42 +77,49 @@ const LeaderboardCard = ({ event_id }) => {
           <Flex
             color="black"
             borderRadius={'md'}
-            px={3}
+            px={{ base: 2, xl: 3 }}
             py={2}
             alignItems={'center'}
             gap={3}
-            fontSize={'lg'}
+            fontSize={{ base: '16px', xl: 'lg' }}
           >
-            <Flex gap={4} alignItems={'center'} w={'9rem'}>
-              <Flex justifyContent={'center'} w={'30px'}>
-                <Text color="#1873FB" fontWeight={'medium'} fontSize={'3xl'}>
+            <Flex gap={{ base: '2', xl: '4' }} alignItems={'center'} w={'9rem'}>
+              <Flex justifyContent={'center'} w={{ base: '14px', xl: '30px' }}>
+                <Text color="#1873FB" fontWeight={'medium'} fontSize={{ base: '20px', xl: '3xl' }}>
                   2
                 </Text>
               </Flex>
-              <Text fontWeight="medium" textAlign="center">
+              <Text fontWeight="medium" fontSize={{ base: '16px', xl: 'lg' }} textAlign="center">
                 {LeaderboardArray[1] && LeaderboardArray[1].volunteer_first_name}{' '}
                 {LeaderboardArray[1] && LeaderboardArray[1].volunteer_last_name}
               </Text>
             </Flex>
-            <Text fontWeight="medium">
+            <Text fontWeight="medium" fontSize={{ base: '14px', xl: 'lg' }}>
               {LeaderboardArray[1] && truncate(LeaderboardArray[1].total_weight, 2)} lb
             </Text>
           </Flex>
 
           {/* 3RD PLACE */}
-          <Flex borderRadius={'md'} px={3} py={2} alignItems={'center'} gap={3} fontSize={'lg'}>
-            <Flex gap={4} alignItems={'center'} w={'9rem'}>
-              <Flex justifyContent={'center'} w={'30px'}>
-                <Text color="#1873FB" fontWeight={'medium'} fontSize={'3xl'}>
+          <Flex
+            borderRadius={'md'}
+            px={{ base: 2, xl: 3 }}
+            py={2}
+            alignItems={'center'}
+            gap={{ base: '2', xl: '3' }}
+            fontSize={{ base: '16px', xl: 'lg' }}
+          >
+            <Flex gap={{ base: '2', xl: '4' }} alignItems={'center'} w={'9rem'}>
+              <Flex justifyContent={'center'} w={{ base: '14px', xl: '30px' }}>
+                <Text color="#1873FB" fontWeight={'medium'} fontSize={{ base: '20px', xl: '3xl' }}>
                   3
                 </Text>
               </Flex>
-              <Text fontWeight="semibold" textAlign="center">
+              <Text fontWeight="semibold" fontSize={{ base: '16px', xl: 'lg' }} textAlign="center">
                 {LeaderboardArray[2] && LeaderboardArray[2].volunteer_first_name}{' '}
                 {LeaderboardArray[2] && LeaderboardArray[2].volunteer_last_name}
               </Text>
             </Flex>
-            <Text fontWeight="semibold">
+            <Text fontWeight="semibold" fontSize={{ base: '14px', xl: 'lg' }}>
               {LeaderboardArray[2] && truncate(LeaderboardArray[2].total_weight, 2)} lb
             </Text>
           </Flex>
@@ -127,7 +139,13 @@ const LeaderboardCard = ({ event_id }) => {
 
   return (
     <>
-      <Card borderRadius="medium" p={3} shadow={'none'}>
+      <Card
+        borderRadius="medium"
+        p={3}
+        shadow={'none'}
+        w={{ base: '288px', xl: '25%' }}
+        h={{ base: '212px', xl: '100%' }}
+      >
         <ActualLeaderboard LeaderboardArray={topThree} />
       </Card>
     </>
