85a11728252ad4f428dabd9f155ffb0d9ea96568
Phillip Cutter <phillipbcutter@gmail.com>
2024-04-05 13:08:31 -0700

    Implemented volunteer event page
    Co-authored-by: Nathan Pietrantonio <theNatePi@users.noreply.github.com>

--- src/App.jsx
+++ src/App.jsx
@@ -26,6 +26,7 @@ import Volunteers from './pages/Volunteers';
 import Navbar from './components/Navbar/Navbar';
 import NavbarDrawer from './components/Navbar/NavbarDrawer';
 import AdminPage from './pages/AdminPage';
+import VolunteerEventPage from './pages/VolunteerEventPage';
 import { RoleProvider } from './utils/RoleContext';
 import { UserProvider } from './utils/UserContext';
 import ProtectedRoute from './utils/ProtectedRoute';
@@ -220,6 +221,14 @@ const App = () => {
                   element={
                     <ProtectedRoute pageType="settings">
                       <DummyProfilePage />
+                    </ProtectedRoute>
+                  }
+                />
+                <Route
+                  path="/volunteer-event"
+                  element={
+                    <ProtectedRoute pageType="volunteer">
+                      <VolunteerEventPage />
                     </ProtectedRoute>
                   }
                 />
---
+++ src/components/Events/FeaturedDashboard.jsx
@@ -0,0 +1,77 @@
+import {
+  Heading,
+  Flex,
+  Grid,
+  GridItem,
+} from '@chakra-ui/react';
+import { useState, useEffect } from 'react';
+import Backend from '../../utils/utils';
+import EventCard from './EventCard';
+
+const FeaturedDashboard = () => {
+  const [featuredEvents, setFeaturedEvents] = useState([]);
+
+  const getEvents = async () => {
+    try {
+      const eventsData = await Backend.get('/events');
+      setFeaturedEvents(eventsData.data.slice(0, 2));
+    } catch (err) {
+      console.log(`Error getting events: `, err.message);
+    }
+  };
+
+  useEffect(() => {
+    getEvents();
+  }, []);
+
+  return (
+    <Flex
+      flexDir={'column'}
+      alignItems={'center'}
+      bg="#E6EAEF"
+      ml={{ base: '0', xl: '15rem' }}
+      pt={10}
+    >
+      <Flex
+        width="95%"
+        pb="8px"
+        flex-direction="column"
+        align-items="center"
+        gap="8px"
+        flex-shrink="0"
+        borderRadius={'xl'}
+        flexDir={'column'}
+      >
+        <Heading fontWeight={900} fontSize={32} fontFamily={'Avenir'} color={'rgba(0, 0, 0, 0.75)'}>
+          Featured Events
+        </Heading>
+      </Flex>
+      <Flex
+        width="95%"
+        pt="10px"
+        flex-direction="column"
+        align-items="center"
+        gap="8px"
+        flex-shrink="0"
+        borderRadius={'xl'}
+        flexDir={'column'}
+      >
+        <Grid templateColumns="repeat(2, 1fr)" gap={6}>
+          {featuredEvents.map(element => (
+            <GridItem key={element.id}>
+              <EventCard
+                {...element}
+                isSelected={false}
+                handleCheckboxChange={() => {}}
+                getEvents={() => {}}
+                isFeatured={true}
+              />
+            </GridItem>
+          ))}
+        </Grid>
+      </Flex>
+    </Flex>
+  );
+};
+
+export default FeaturedDashboard;
