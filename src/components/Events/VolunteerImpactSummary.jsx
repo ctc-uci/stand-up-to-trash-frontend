1a9a30f605dfcf2e6c39fc05ccd6bc83dd395a24
Nathan Pietrantonio <pietrann@uci.edu>
2024-04-14 19:03:09 -0700

    Implement functionality for ImpactSummary

--- src/components/Events/VolunteerImpactSummary.jsx
+++ src/components/Events/VolunteerImpactSummary.jsx
@@ -1,8 +1,9 @@
 import { Box, Flex } from '@chakra-ui/react';
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useContext } from 'react';
 import { FaTrashCan } from 'react-icons/fa6';
 import { IoDocumentText } from 'react-icons/io5';
 import { MdPeopleAlt } from 'react-icons/md';
+import UserContext from '../../utils/UserContext';
 import Backend from '../../utils/utils';
 import DataCard from './DataCard';
 
@@ -11,34 +12,46 @@ const VolunteerImpactSummary = () => {
   const [total, setTotalPounds] = useState(0);
   const [largestItem, setLargestItem] = useState(0);
 
+  const { user, updateUser } = useContext(UserContext);
+
   // mock volunteer object so i can push for now; delete after
-  const volunteer = {
-    id: '123',
-    name: 'John Doe',
-    email: 'johndoe@example.com',
-    ounces: '5',
-    pounds: '10'
-  };
+  // const volunteer = {
+  //   id: '123',
+  //   name: 'John Doe',
+  //   email: 'johndoe@example.com',
+  //   ounces: '5',
+  //   pounds: '10'
+  // };
 
   useEffect(() => {
+    updateUser();
+    console.log(user);
     getData();
   }, []);
 
   const getData = async () => {
     try {
-    let totalPounds = 0; 
-      // need to get and set the volunteer id here
-      let response = await Backend.get(`/data/volunteer/${volunteer.id}/event`);
-      console.log(response.data.length);
-      setEvents(response.data.length);
-      response = await Backend.get(`/data/volunteer/${volunteer.id}`);
-      for (const { ounces, pounds } of response.data) { totalPounds += ounces / 16 + pounds; }
-      console.log(totalPounds);
-    setTotalPounds(parseFloat(totalPounds));
-      response = await Backend.get(`/data/volunteer/${volunteer.id}`);
-      console.log(response.data);
-      // lol need to fix; this is prolly going to error
-      setLargestItem(parseFloat(['max_weight']));
+      let totalPounds = 0; 
+      let allWeights = [];
+      // need to get and set the volunteer id here ???
+      // let userId = 111; // For testing
+      let userId = user?.id;
+
+      let eventResponse = await Backend.get(`/data/volunteer/${userId}/event`);
+      setEvents(eventResponse.data.length);
+      console.log(eventResponse);
+    
+      let weightResponse = await Backend.get(`/data/volunteer/${userId}`);
+      for (const { ounces, pounds } of weightResponse.data) { totalPounds += ounces / 16 + pounds; allWeights.push(ounces / 16 + pounds); }
+      setTotalPounds(parseFloat(totalPounds));
+
+      let largestItem = parseFloat(Math.max(...allWeights));
+      console.log(allWeights);
+      if (isNaN(largestItem) || !isFinite(largestItem)) {
+        largestItem = 0;
+      }
+      console.log(largestItem)
+      setLargestItem(largestItem);
     } catch (err) {
       console.log(`Error getting events: `, err.message);
     }
