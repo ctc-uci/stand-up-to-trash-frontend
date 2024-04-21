77e504fd4cceef9003bcdbe27d379eaecc627868
Emmy Chen <emmyc3@uci.edu>
2024-04-21 15:47:13 -0700

    Add Heading and yassify formatting for laptop and mobile to match figma hopefully it works

--- src/components/Events/FeaturedDashboard.jsx
+++ src/components/Events/FeaturedDashboard.jsx
@@ -1,11 +1,11 @@
-import {Heading, Flex, Grid, GridItem, IconButton, useBreakpointValue} from '@chakra-ui/react';
+import { Heading, Flex, Grid, GridItem, IconButton, useBreakpointValue } from '@chakra-ui/react';
 import { useState, useEffect } from 'react';
 import Backend from '../../utils/utils';
 import EventCard from './EventCard';
-import {RxCaretLeft} from 'react-icons/rx';
+import { RxCaretLeft } from 'react-icons/rx';
 import PropTypes from 'prop-types';
 
-const FeaturedDashboard = ({onOpen, showOpenDrawerButton}) => {
+const FeaturedDashboard = ({ onOpen, showOpenDrawerButton }) => {
   const [featuredEvents, setFeaturedEvents] = useState([]);
   const numEvents = useBreakpointValue({ base: 1, md: 2, xl: 2 });
 
@@ -27,42 +27,52 @@ const FeaturedDashboard = ({onOpen, showOpenDrawerButton}) => {
       flexDir={'column'}
       alignItems={'center'}
       bg="#E6EAEF"
-      ml={{ base: '0', xl: '15rem' }}
+      ml={{ base: '3vw', xl: '15rem' }}
+      mr={{ base: '3vw' }}
       pt={4}
     >
       <Flex
-        width="95%"
+        width={{ base: '100%', xl: '95%' }}
+        p={{ base: '20px' }}
         pb="8px"
         align-items="center"
-        justifyContent={"space-between"}
+        justifyContent={'space-between'}
         gap="8px"
         flex-shrink="0"
         borderRadius={'xl'}
         flexDir={'row'}
       >
-        <Flex w="70%">
-          <Heading fontWeight={900} fontSize={32} fontFamily={'Avenir'} color={'rgba(0, 0, 0, 0.75)'}>
+        <Flex w="80%">
+          <Heading
+            w="100%"
+            fontWeight={{ base: 600, xl: 900 }}
+            lineHeight="normal"
+            fontStyle="normal"
+            fontSize={{ base: '18px', xl: '32px' }}
+            fontFamily={'Avenir'}
+            color={'rgba(0, 0, 0, 0.75)'}
+          >
             Featured Events
           </Heading>
         </Flex>
         <Flex w="40px">
           <IconButton
-                borderRadius="md"
-                borderColor="#EFEFEF"
-                bg="white"
-                variant={"outline"}
-                borderWidth={'0.2em'}
-                h="40px"
-                w="40px"
-                icon={<RxCaretLeft size={22}/>}
-                onClick={onOpen}
-                display={showOpenDrawerButton ? { base: 'none', xl: 'flex' } : 'none'}
-
+            borderRadius="md"
+            borderColor="#EFEFEF"
+            bg="white"
+            variant={'outline'}
+            borderWidth={'0.2em'}
+            h="40px"
+            w="40px"
+            icon={<RxCaretLeft size={22} />}
+            onClick={onOpen}
+            display={showOpenDrawerButton ? { base: 'none', xl: 'flex' } : 'none'}
           ></IconButton>
         </Flex>
       </Flex>
       <Flex
-        width="95%"
+        width="96%"
+        ml={{ xl: 3 }}
         pt="10px"
         flex-direction="column"
         align-items="center"
@@ -71,7 +81,10 @@ const FeaturedDashboard = ({onOpen, showOpenDrawerButton}) => {
         borderRadius={'xl'}
         flexDir={'column'}
       >
-        <Grid templateColumns={{base:"repeat(1, 1fr)", md: "repeat(2, 1fr)", xl:"repeat(2, 1fr)"}} gap={6}>
+        <Grid
+          templateColumns={{ base: 'repeat(1, 1fr)', md: 'repeat(2, 1fr)', xl: 'repeat(2, 1fr)' }}
+          gap={6}
+        >
           {featuredEvents.map(element => (
             <GridItem key={element.id}>
               <EventCard
