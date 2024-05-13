a5559e781bfe0ccc57a334e5bec1d479b3ca7124
Emmy Chen <emmyc3@uci.edu>
2024-05-12 19:09:03 -0700

    finish read more

--- src/components/VolunteerSideView.jsx
+++ src/components/VolunteerSideView.jsx
@@ -10,7 +10,7 @@ import {
   useDisclosure,
 } from '@chakra-ui/react';
 import PropTypes from 'prop-types';
-import { useState, useEffect, useContext } from 'react';
+import { useState, useEffect } from 'react';
 import { Icon } from '@chakra-ui/react';
 import { getEventById } from '../utils/eventsUtils';
 import { EditIcon, CalendarIcon } from '@chakra-ui/icons';
@@ -21,30 +21,18 @@ import { IoMdLink } from 'react-icons/io';
 import { RxCaretRight } from 'react-icons/rx';
 import HappeningInChip from '../components/HappeningInChip/HappeningInChip';
 import RegistrationFlowController from '../components/EventRegistration/RegistrationFlowController.jsx';
-import ical, { ICalCalendarMethod } from 'ical-generator';
-import UserContext from '../utils/UserContext.jsx';
-import { getEventDataVolunteerId } from '../utils/eventsUtils';
 
 const VolunteerSideView = ({ eventId, onClose, setShowOpenDrawerButton }) => {
-  const [eventData, setEventData] = useState();
+  const [eventData, setEventData] = useState([]);
   const [isReadMore, setIsReadMore] = useState(false);
-  // const [calendarSelected, setCalendarSelected] = useState(false);
+  const [showReadMore, setShowReadMore] = useState(false);
+  const [calendarSelected, setCalendarSelected] = useState(false);
   const [mapSelected, setMapSelected] = useState(false);
-  const [dateObj, setDateObj] = useState(new Date());
-
-  // only parse the date if eventData has been retrieved
-  useEffect(() => {
-    if (eventData) {
-      setDateObj(new Date(Date.parse(eventData.date)));
-    }
-  }, [eventData]);
-  const [eventDataVolunteer, setEventDataVolunteer] = useState([]);
-  const { user } = useContext(UserContext);
-  console.log('users', user);
-  console.log('eventid', eventDataVolunteer[0]);
-
+  const slicedDescription = !isReadMore
+    ? eventData?.description?.slice(0, 200)
+    : eventData?.description;
   // const [dateObj, setDateObj] = useState(new Date());
-  // const dateObj = new Date(Date.parse(eventData.date));
+  const dateObj = new Date(Date.parse(eventData.date));
   // console.log(eventData);
 
   const {
@@ -53,83 +41,19 @@ const VolunteerSideView = ({ eventId, onClose, setShowOpenDrawerButton }) => {
     onClose: onRegistrationFlowClose,
   } = useDisclosure();
 
-  // At the top of your component
+  useEffect(() => {
+    getEventById(eventId).then(data => setEventData(data));
+    setIsReadMore(false);
+    // setDateObj(new Date(Date.parse(eventData.date)))
+  }, [eventId]);
 
   useEffect(() => {
-    if (eventId) {
-      getEventById(eventId)
-        .then(data => {
-          setEventData(data); // Make sure data is an object with the properties you expect
-        })
-        .catch(error => {
-          console.error('Failed to fetch event data:', error);
-        });
-      if (user.id && eventId) {
-        getEventDataVolunteerId(user.id, eventId)
-          .then(data => setEventDataVolunteer(data))
-          .catch(error => console.error('Failed to fetch event volunteer data:', error));
-      }
+    if (eventData.description) {
+      setShowReadMore(eventData.description.length > 200);
     }
-  }, [eventId, user.id]);
+  }, [eventData.description]);
 
-  // console.log('this is the event id:', eventId);
-  console.log('this is the', eventData);
-  // console.log('d', dateObj)
-  //* download ics file by calling backend
-  // Assuming `eventData` is part of the state as shown previously
-  const calendar = ical({ name: 'my first iCal' });
-  calendar.method(ICalCalendarMethod.REQUEST);
 
-  const handleDownloadICS = async () => {
-    // const baseURL = window.location.origin;
-    // const eventURL = `${baseURL}/events/${eventId}`;
-    // TODO: using eventData.date, eventData.start_time, eventData.end_time
-    // format a start time that looks like eventData.date
-    // extract the date from the eventData
-    const datePart = eventData.date.split('T')[0];
-    // TODO: I'm not sure if the time zone in the data is aligned to the pst timezone so I will leave it as is for now.
-    const formatted_start_time = `${datePart}T${eventData.start_time.substring(0, 8)}`;
-
-    const formatted_end_time = `${datePart}T${eventData.end_time.substring(0, 8)}`;
-
-    // test the dates are correct
-    console.log('the is date', eventData.date, 'but the part we want is', datePart);
-    console.log('ICS start time:', formatted_start_time, 'ICS End time:', formatted_end_time);
-    console.log('real start time:', eventData.start_time, 'real end time:', eventData.end_time);
-    // const calendarFile = new File([calendar.toString()], 'event.ics', { type: 'text/calendar' });
-    // const url = URL.createObjectURL(calendarFile);
-    // setICSURL(url);
-    // TODO: please check that the url is correct
-
-    calendar.createEvent({
-      start: formatted_start_time,
-      end: formatted_end_time,
-      summary: eventData.name,
-      description: eventData.description,
-      location: eventData.location,
-      url: window.location.href,
-    });
-
-    const calendarData = calendar.toString();
-    console.log(eventData.description, eventData.name, eventData.location, window.location.href);
-    const blob = new Blob([calendarData], { type: 'text/calendar;charset=utf-8' });
-    const url = URL.createObjectURL(blob);
-
-    // Create and trigger a download link
-    const link = document.createElement('a');
-    link.href = url;
-    link.setAttribute('download', 'event.ics');
-    document.body.appendChild(link);
-    link.click();
-    document.body.removeChild(link);
-  };
-  // const handleClose = () => {
-  //   onClose(); // Ensure onClose is always called to close the view
-  //   setShowOpenDrawerButton(true);
-  // };
-
-  // setDateObj(new Date(Date.parse(eventData.date)))
-  // }, [eventId]);
 
   // console.log('e', eventData);
   // console.log('d', dateObj)
@@ -162,9 +86,8 @@ const VolunteerSideView = ({ eventId, onClose, setShowOpenDrawerButton }) => {
 
     return `${month} ${day}, ${year} @ ${time}`;
   }
-  return eventData ? (
-    // Flag: Need responsive
-    <Flex flexDir={'column'} w={{ base: '100vw', xl: '23vw' }} mt={'1em'} mx={'20px'}>
+  return (
+    <Flex flexDir={'column'} w={'26em'} mt={'1em'} mx={'20px'}>
       <HStack justify={'center'} align={'center'}>
         <IconButton
           borderRadius="md"
@@ -228,7 +151,7 @@ const VolunteerSideView = ({ eventId, onClose, setShowOpenDrawerButton }) => {
             gap={'0.3em'}
           >
             <IoPeopleSharp color="purple" />
-            <Text>{eventDataVolunteer[0]?.number_in_party > 1 ? 'Group' : 'Individual'}</Text>
+            <Text>Group</Text>
           </Flex>
         </Flex>
         <Flex justify={'space-between'} alignItems={'center'}>
@@ -244,12 +167,12 @@ const VolunteerSideView = ({ eventId, onClose, setShowOpenDrawerButton }) => {
             gap={'0.3em'}
           >
             <CalendarIcon color="purple" />
-            <Text>{eventDataVolunteer[0]?.is_checked_in ? 'Checked-in' : 'Registered'}</Text>
+            <Text>Registered</Text>
           </Flex>
         </Flex>
         <Flex justify={'space-between'} alignItems={'center'}>
           <Text>Party size</Text>
-          <Text fontWeight={'bold'}>{eventDataVolunteer[0]?.number_in_party}</Text>
+          <Text fontWeight={'bold'}>12 people</Text>
         </Flex>
       </Box>
 
@@ -269,9 +192,9 @@ const VolunteerSideView = ({ eventId, onClose, setShowOpenDrawerButton }) => {
         <Text fontWeight={'medium'} color={'gray'} fontSize={15} textAlign={'start'} width={'full'}>
           {formatDate(dateObj)}
         </Text>
-        <Text noOfLines={isReadMore ? null : 3}>{eventData.description}</Text>
+        <Text>{slicedDescription}</Text>
         <Flex w={'100%'}>
-          {!isReadMore && (
+          {!isReadMore && showReadMore && (
             <Text
               color={'#0075FF'}
               fontWeight={600}
@@ -279,7 +202,7 @@ const VolunteerSideView = ({ eventId, onClose, setShowOpenDrawerButton }) => {
               _hover={{
                 cursor: 'pointer',
               }}
-              onClick={() => setIsReadMore(true)}
+              onClick={() => setIsReadMore(!isReadMore)}
             >
               Read more...
             </Text>
@@ -301,29 +224,32 @@ const VolunteerSideView = ({ eventId, onClose, setShowOpenDrawerButton }) => {
         <Flex gap={'1em'}>
           <Flex
             backgroundColor={'#EFEFEF'}
-            w={'20vw'}
-            padding={'1vh'}
-            align={'center'}
+            w={'12.5em'}
+            padding={'0.8em'}
             borderRadius={'0.5em'}
             justify={'space-between'}
-            onClick={handleDownloadICS}
+            align={'center'}
+            onClick={() => setCalendarSelected(prev => !prev)}
+            borderColor={calendarSelected ? 'blue.200' : '#EFEFEF'}
             borderWidth={2}
           >
             <Flex justify={'center'} align={'center'}>
-              <Image src={logos_google_calendar} h={'1.5vw'} w={'1.5vw'} mr={'1vw'} />
-              <Text fontWeight={600} mr={'1vw'}>
-                Calendar
-              </Text>
+              <Image src={logos_google_calendar} h={'1.3em'} w={'1.3em'} mr={'9%'} />
+              <Text fontWeight={600}>Calendar</Text>
             </Flex>
             <Flex justify={'center'} align={'center'}>
-              <IconButton as={IoMdLink} h={'1.5vw'} w={'1.5vw'} backgroundColor={'#EFEFEF'} />
+              <IconButton
+                as={IoMdLink}
+                h={'1.3em'}
+                w={'1.3em'}
+                backgroundColor={calendarSelected ? 'blue.200' : '#EFEFEF'}
+              />
             </Flex>
           </Flex>
-
           <Flex
             backgroundColor={'#EFEFEF'}
-            w={'20vw'}
-            padding={'1vh'}
+            w={'12.5em'}
+            padding={'0.8em'}
             align={'center'}
             borderRadius={'0.5em'}
             justify={'space-between'}
@@ -336,16 +262,16 @@ const VolunteerSideView = ({ eventId, onClose, setShowOpenDrawerButton }) => {
             borderWidth={2}
           >
             <Flex justify={'center'} align={'center'}>
-              <Image src={logos_google_maps} h={'1.5vw'} w={'1.5vw'} mr={'0.5vw'} />
-              <Text fontWeight={600} mr={'0.5vw'}>
+              <Image src={logos_google_maps} h={'1.3em'} w={'1.3em'} mr={'9%'} />
+              <Text fontWeight={600} w={'8em'}>
                 Google Map
               </Text>
             </Flex>
-            <Flex justify={'center'} align={'center'}>
+            <Flex justify={'center'} align={'center'} h={'1.3em'} w={'1.3em'}>
               <IconButton
                 as={IoMdLink}
-                h={'1.5vw'}
-                w={'1.5vw'}
+                h={'1.3em'}
+                w={'1.3em'}
                 backgroundColor={mapSelected ? 'blue.200' : '#EFEFEF'}
               />
             </Flex>
@@ -364,8 +290,6 @@ const VolunteerSideView = ({ eventId, onClose, setShowOpenDrawerButton }) => {
         />
       )}
     </Flex>
-  ) : (
-    <Text>Loading Event...</Text>
   );
 };
 
@@ -374,4 +298,5 @@ VolunteerSideView.propTypes = {
   onClose: PropTypes.func.isRequired,
   setShowOpenDrawerButton: PropTypes.func.isRequired,
 };
+
 export default VolunteerSideView;
